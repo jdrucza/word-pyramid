b6b313f0d8a7b9c38f5a045c2d52f86e250cf127
James Drucza <james@drucza.net>
2013-03-02 10:07:02 +1100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WordPyramid::Application.config.secret_token = 'c8c0f709e1fbd6265c2984ba3370c33c3303e712ff744f0ea753166eaea68ced9e67274f20e566d28ec45e4f01ac622ca89b404f9119c2de029f7f401278591a'
