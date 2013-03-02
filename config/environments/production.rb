bdc74b652439cb4082be1bb30fdc14199c3f6c93
James Drucza <james@drucza.net>
2013-03-03 04:55:11 +1100

    raising email errors in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ WordPyramid::Application.configure do
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true # TODO this should probably be false in prod
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
